e0344b0b3f4047816369e19e41fc45f82008dede
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-02-20 23:55:07 +0300

    add colors

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import pygame as pg
 
-
+blue_color = (161, 234, 251)
+white_color = (253, 253, 253)
+pink_color = (255, 206, 243)
+purple_color = (202, 187, 233)
 display_size = (1000, 600)
 display = pg.display.set_mode(display_size)
 pg.init()
 pg.display.set_caption('Present')
+
+
+def start():
+    pass
